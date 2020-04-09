36d8297b537dcd035ed565c1bb0e45c0bb7f3511
JGRM <german9989@outlook.com>
2020-04-09 18:00:34 -0500

    service page and color

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
+
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
   });
