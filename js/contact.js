c3a0684e74a5f61cd4196f4a9b44c8cf124c856c
JGRM <german9989@outlook.com>
2020-04-20 09:42:42 -0500

    fimal details on web

--- js/contact.js
+++ js/contact.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+
+  document.getElementById('submit', function() {
+    alert('Your information was sent, Thank you!')
+  })
